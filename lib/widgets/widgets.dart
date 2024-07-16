9fe3d68c48ee838eba5fc76a839281e933f6a42b
Sachinthana <dinithisachinthana@gmail.com>
2024-07-16 07:40:39 +0530

    created common colour widget

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,7 +2,5 @@ export 'login_page/login_widget.dart';
 export 'common/user_profile.dart';
 export 'common/theme.dart';
 export 'common/student_bg.dart';
-
-//export 'common/table.dart';
-
+export 'common/colours.dart';
 
