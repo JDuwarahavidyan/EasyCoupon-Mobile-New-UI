08d6888a189573bacfe2ad69582a9257372b4bcb
Sachinthana <dinithisachinthana@gmail.com>
2024-07-17 11:28:06 +0530

    created login UI

--- lib/pages/login_pages/login_page.dart
+++ lib/pages/login_pages/login_page.dart
@@ -38,9 +38,8 @@ class _SignInThreeState extends State<SignInThree> {
               ),
 
               Positioned(
-                // left: -26,
-                top: 51.0,
-                bottom: 12,
+                left: -26,               
+                bottom: 100,
                 child: SvgPicture.string(
                   '<svg viewBox="-26.0 51.0 91.92 91.92" ><path transform="matrix(0.707107, -0.707107, 0.707107, 0.707107, -26.0, 96.96)" d="M 48.75 0 L 65 32.5 L 48.75 65 L 16.24999809265137 65 L 0 32.5 L 16.25000381469727 0 Z" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="matrix(0.707107, -0.707107, 0.707107, 0.707107, -10.83, 105.24)" d="M 0 0 L 27.625 11.05000019073486 L 55.25 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="matrix(0.707107, -0.707107, 0.707107, 0.707107, 16.51, 93.51)" d="M 0 37.04999923706055 L 0 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
                   width: 91.92,
@@ -57,6 +56,18 @@ class _SignInThreeState extends State<SignInThree> {
                   height: 268.27,
                 ),
               ),
+
+                Positioned(
+                right: 42.0,
+                bottom: -120,
+                child: SvgPicture.string(
+                  '<svg viewBox="63.0 -103.0 268.27 268.27" ><path transform="matrix(0.5, -0.866025, 0.866025, 0.5, 63.0, 67.08)" d="M 147.2896423339844 0 L 196.3861999511719 98.19309997558594 L 147.2896423339844 196.3861999511719 L 49.09654235839844 196.3861999511719 L 0 98.19309234619141 L 49.09656143188477 0 Z" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="matrix(0.5, -0.866025, 0.866025, 0.5, 113.73, 79.36)" d="M 0 0 L 83.46413421630859 33.38565444946289 L 166.9282684326172 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="matrix(0.5, -0.866025, 0.866025, 0.5, 184.38, 23.77)" d="M 0 111.9401321411133 L 0 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
+                  width: 268.27,
+                  height: 268.27,
+                ),
+              ),
+
+
               Positioned(
                 right: -19,
                 top: 31.0,
@@ -66,6 +77,18 @@ class _SignInThreeState extends State<SignInThree> {
                   height: 65.0,
                 ),
               ),
+
+              
+              Positioned(
+                right: -19,
+                bottom: 331.0,
+                child: SvgPicture.string(
+                  '<svg viewBox="329.0 31.0 65.0 65.0" ><path transform="translate(329.0, 31.0)" d="M 48.75 0 L 65 32.5 L 48.75 65 L 16.24999809265137 65 L 0 32.5 L 16.25000381469727 0 Z" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(333.88, 47.58)" d="M 0 0 L 27.625 11.05000019073486 L 55.25 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(361.5, 58.63)" d="M 0 37.04999923706055 L 0 0" fill="none" stroke="#ffffff" stroke-width="1" stroke-opacity="0.25" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
+                  width: 65.0,
+                  height: 65.0,
+                ),
+              ),
+
 
               //card and footer ui
               Positioned(
@@ -171,7 +194,7 @@ class _SignInThreeState extends State<SignInThree> {
       TextSpan(
         style: GoogleFonts.inter(
           fontSize: fontSize,
-          color: const Color(0xFF21899C),
+          color: AppColors.landColor,
           letterSpacing: 2.000000061035156,
         ),
         children: const [
@@ -184,7 +207,7 @@ class _SignInThreeState extends State<SignInThree> {
           TextSpan(
             text: 'IN',
             style: TextStyle(
-              color: Color(0xFFFE9879),
+              color:  AppColors.secondaryColor,
               fontWeight: FontWeight.w800,
             ),
           ),
@@ -215,25 +238,26 @@ class _SignInThreeState extends State<SignInThree> {
             ),
             filled: true,
             fillColor: emailController.text.isEmpty
-                ? const Color.fromRGBO(248, 247, 251, 1)
-                : Colors.transparent,
+                ? AppColors.shadowColor
+                : AppColors.textColor,
+            //contentPadding: const EdgeInsets.only(left: 30.0, top: 20.0),
             enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(40),
                 borderSide: BorderSide(
                   color: emailController.text.isEmpty
-                      ? Colors.transparent
-                      : const Color.fromRGBO(44, 185, 176, 1),
+                      ? Color.fromARGB(0, 0, 0, 0)
+                      : AppColors.landColor,
                 )),
             focusedBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(40),
                 borderSide: const BorderSide(
-                  color: Color.fromRGBO(44, 185, 176, 1),
+                  color: AppColors.landColor,
                 )),
             prefixIcon: Icon(
               Icons.mail_outline_rounded,
               color: emailController.text.isEmpty
                   ? const Color(0xFF151624).withOpacity(0.5)
-                  : const Color.fromRGBO(44, 185, 176, 1),
+                  :AppColors.landColor,
               size: 16,
             ),
             suffix: Container(
@@ -242,13 +266,13 @@ class _SignInThreeState extends State<SignInThree> {
               height: 24.0,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(100.0),
-                color: const Color.fromRGBO(44, 185, 176, 1),
+                color:AppColors.landColor,
               ),
               child: emailController.text.isEmpty
                   ? const Center()
                   : const Icon(
                       Icons.check,
-                      color: Colors.white,
+                      color: AppColors.primaryColor,
                       size: 13,
                     ),
             ),
@@ -258,129 +282,114 @@ class _SignInThreeState extends State<SignInThree> {
     );
   }
 
-  Widget passwordTextField(Size size) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: SizedBox(
-        height: size.height / 12,
-        child: TextField(
-          controller: passController,
-          style: GoogleFonts.inter(
-            fontSize: 16.0,
-            color: const Color(0xFF151624),
-          ),
-          cursorColor: const Color(0xFF151624),
-          obscureText: true,
-          keyboardType: TextInputType.visiblePassword,
-          decoration: InputDecoration(
-            hintText: 'Enter your password',
-            hintStyle: GoogleFonts.inter(
-              fontSize: 16.0,
-              color: const Color(0xFF151624).withOpacity(0.5),
-            ),
-            filled: true,
-            fillColor: passController.text.isEmpty
-                ? const Color.fromRGBO(248, 247, 251, 1)
-                : Colors.transparent,
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(40),
-                borderSide: BorderSide(
-                  color: passController.text.isEmpty
-                      ? Colors.transparent
-                      : const Color.fromRGBO(44, 185, 176, 1),
-                )),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(40),
-                borderSide: const BorderSide(
-                  color: Color.fromRGBO(44, 185, 176, 1),
-                )),
-            prefixIcon: Icon(
-              Icons.lock_outline_rounded,
-              color: passController.text.isEmpty
-                  ? const Color(0xFF151624).withOpacity(0.5)
-                  : const Color.fromRGBO(44, 185, 176, 1),
-              size: 16,
-            ),
-            suffix: Container(
-              alignment: Alignment.center,
-              width: 24.0,
-              height: 24.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100.0),
-                color: const Color.fromRGBO(44, 185, 176, 1),
-              ),
-              child: passController.text.isEmpty
-                  ? const Center()
-                  : const Icon(
-                      Icons.check,
-                      color: Colors.white,
-                      size: 13,
-                    ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget signInButton(Size size) {
-    return Container(
-      alignment: Alignment.center,
-      height: size.height / 13,
-      width: size.width * 0.7,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50.0),
-        color: const Color(0xFF21899C),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0xFF4C2E84).withOpacity(0.2),
-            offset: const Offset(0, 15.0),
-            blurRadius: 60.0,
-          ),
-        ],
-      ),
-      child: Text(
-        'Sign in',
+Widget passwordTextField(Size size) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+    child: SizedBox(
+      height: size.height / 12,
+      child: TextField(
+        controller: passController,
         style: GoogleFonts.inter(
           fontSize: 16.0,
-          color: Colors.white,
-          fontWeight: FontWeight.w600,
-          height: 1.5,
+          color: const Color(0xFF151624),
         ),
-        textAlign: TextAlign.center,
+        cursorColor: const Color(0xFF151624),
+        obscureText: true,
+        keyboardType: TextInputType.visiblePassword,
+        decoration: InputDecoration(
+          labelText: 'Enter your password',
+          labelStyle: GoogleFonts.inter(
+            fontSize: 16.0,
+            color: const Color(0xFF151624).withOpacity(0.5),
+          ),
+          filled: true,
+          fillColor: passController.text.isEmpty
+              ?AppColors.shadowColor
+              : Colors.transparent,
+          //contentPadding: const EdgeInsets.only(left: 25.0, top: 20.0), // Adjust left and top padding here
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40),
+            borderSide: BorderSide(
+              color: passController.text.isEmpty
+                  ? Colors.transparent
+                  : AppColors.landColor,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40),
+            borderSide: const BorderSide(
+              color: AppColors.landColor,
+            ),
+          ),
+          prefixIcon: Icon(
+            Icons.lock_outline_rounded,
+            color: passController.text.isEmpty
+                ? const Color(0xFF151624).withOpacity(0.5)
+                : AppColors.landColor,
+            size: 16,
+          ),
+          suffix: Container(
+            alignment: Alignment.center,
+            width: 24.0,
+            height: 24.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100.0),
+              color: AppColors.landColor,
+            ),
+            child: passController.text.isEmpty
+                ? const Center()
+                : const Icon(
+                    Icons.check,
+                    color: AppColors.primaryColor,
+                    size: 13,
+                  ),
+          ),
+        ),
       ),
-    );
-  }
+    ),
+  );
+}
+
+
+
+Widget signInButton(Size size) {
+  return TextButton(
+    style: TextButton.styleFrom(
+      padding: EdgeInsets.symmetric(vertical: size.height / 26, horizontal: size.width * 0.35),
+      backgroundColor: AppColors.landColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      shadowColor: const Color(0xFF4C2E84).withOpacity(0.2),
+      elevation: 5.0,
+    ),
+    onPressed: () {
+      // Add your onPressed logic here
+    },
+    child: Text(
+      'Sign in',
+      style: GoogleFonts.inter(
+        fontSize: 16.0,
+        color: AppColors.primaryColor,
+        fontWeight: FontWeight.w600,
+        height: 1.5,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
 
   Widget buildRememberForgetSection() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Row(
         children: [
-          Container(
-            alignment: Alignment.center,
-            width: 20.0,
-            height: 20.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.0),
-              color: const Color(0xFF21899C),
-            ),
-            child: const Icon(
-              Icons.check,
-              size: 13,
-              color: Colors.white,
-            ),
-          ),
+        
           const SizedBox(
             width: 8,
           ),
-          // Text(
-          //   'Remember me',
-          //   style: GoogleFonts.inter(
-          //     fontSize: 15.0,
-          //     color: const Color(0xFF0C0D34),
-          //   ),
-          // ),
+         
           const Spacer(),
        TextButton(
   onPressed: () {
@@ -390,7 +399,7 @@ class _SignInThreeState extends State<SignInThree> {
     'Forgot password',
     style: GoogleFonts.inter(
       fontSize: 13.0,
-      color: const Color(0xFF21899C),
+      color: AppColors.landColor,
       fontWeight: FontWeight.w500,
     ),
     textAlign: TextAlign.right,
--- lib/widgets/common/colours.dart
+++ lib/widgets/common/colours.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color secondaryColor = Color(0xFFDD6B4D);
   static const Color textColor = Color(0xFF000000);
   static const Color splashColor = Color(0xFFFF9822);
-  static const Color shadowColor = Color(0xFFDAE1E7);
+  static const Color shadowColor = Color.fromARGB(255, 228, 236, 242);
   static const Color buttonColor = Color(0xff132137);
   static const Color landColor = Color(0xff183661);
 
