e780b5b9fb013f23decf74d72b70bd7c95d67fe6
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2024-08-24 08:23:04 +0530

    Minor Changes

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,5 +1,5 @@
 class RouteNames {
-  static const String splash = '/splash';
+  static const String splash = '/';
   static const String introductionAnimation = '/introductionAnimation';
-  static const String login = '/';
+  static const String login = '/login';
 }
