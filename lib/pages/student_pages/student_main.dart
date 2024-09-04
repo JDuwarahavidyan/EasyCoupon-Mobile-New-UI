2b7ad581fbd1824168de307fd7b872c8eec97bdf
Sachinthana <dinithisachinthana@gmail.com>
2024-09-04 15:49:29 +0530

    solved issue in nav bar

--- lib/pages/student_pages/student_main.dart
+++ lib/pages/student_pages/student_main.dart
@@ -1,66 +1,7 @@
-// import 'package:easy_coupon/pages/pages.dart';
-// import 'package:easy_coupon/pages/student_pages/student_report.dart';
-// import 'package:easy_coupon/widgets/widgets.dart';
-// import 'package:flutter/material.dart';
-
-// class StudentMainPage extends StatefulWidget {
-//   @override
-//   _StudentMainPageState createState() => _StudentMainPageState();
-// }
-
-// class _StudentMainPageState extends State<StudentMainPage> {
-//   int _currentIndex = 0;
-
-//   final List<Widget> _pages = [
-//     StudentHome(),
-//     Student_report(),
-//     ProfileScreen(),
-//     // Add more pages as needed
-//   ];
-
-//   void _onTabSelected(int index) {
-//     setState(() {
-//       _currentIndex = index;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           AnimatedSwitcher(
-//             duration: Duration(milliseconds: 300),
-//             child: _pages[_currentIndex],
-//           ),
-//           Positioned(
-//             bottom: 0,
-//             left: 0,
-//             right: 0,
-//             child: BottomBarView(
-//               currentIndex: _currentIndex,
-//               onTabSelected: _onTabSelected,
-//               tabIconsList: [],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
 import 'package:easy_coupon/pages/pages.dart';
 import 'package:easy_coupon/pages/student_pages/student_report.dart';
 import 'package:easy_coupon/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:easy_coupon/pages/custom_page_route.dart'; // Import the custom transition
 
 class StudentMainPage extends StatefulWidget {
   @override
@@ -74,16 +15,10 @@ class _StudentMainPageState extends State<StudentMainPage> {
     StudentHome(),
     Student_report(),
     ProfileScreen(),
+    // Add more pages as needed
   ];
 
   void _onTabSelected(int index) {
-    final bool slideLeftToRight = _currentIndex > index;
-
-    Navigator.of(context).pushReplacement(CustomPageTransition(
-      page: _pages[index],
-      slideLeftToRight: slideLeftToRight,
-    ));
-
     setState(() {
       _currentIndex = index;
     });
@@ -92,11 +27,23 @@ class _StudentMainPageState extends State<StudentMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomBarView(
-        currentIndex: _currentIndex,
-        onTabSelected: _onTabSelected,
-        tabIconsList: [],
+      body: Stack(
+        children: [
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 300),
+            child: _pages[_currentIndex],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: BottomBarView(
+              currentIndex: _currentIndex,
+              onTabSelected: _onTabSelected,
+              tabIconsList: [],
+            ),
+          ),
+        ],
       ),
     );
   }
