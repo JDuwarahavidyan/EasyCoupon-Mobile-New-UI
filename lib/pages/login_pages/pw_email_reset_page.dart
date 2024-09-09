a8d90df5b81e5b889ecd81c5585f60c2c29c37e2
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2024-09-10 00:04:48 +0530

    Implemented the backend for reset password with email page

--- lib/pages/login_pages/pw_email_reset_page.dart
+++ lib/pages/login_pages/pw_email_reset_page.dart
@@ -1,16 +1,26 @@
-import 'package:easy_coupon/routes/route_names.dart';
+import 'package:easy_coupon/bloc/blocs.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_coupon/routes/route_names.dart';
 import 'package:easy_coupon/widgets/widgets.dart';
+import 'package:floating_snackbar/floating_snackbar.dart';
 
-class PasswordEmailResetPage extends StatelessWidget {
-    const PasswordEmailResetPage({super.key});
+class PasswordEmailResetPage extends StatefulWidget {
+  const PasswordEmailResetPage({super.key});
+
+  @override
+  _PasswordEmailResetPageState createState() => _PasswordEmailResetPageState();
+}
+
+class _PasswordEmailResetPageState extends State<PasswordEmailResetPage> {
+  final TextEditingController emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,  // Prevents background from moving when keyboard appears
+      resizeToAvoidBottomInset: false,
       body: Background(
         child: Stack(
           children: <Widget>[
@@ -19,9 +29,9 @@ class PasswordEmailResetPage extends StatelessWidget {
               left: 20,
               child: GestureDetector(
                 onTap: () {
-                   Navigator.pushReplacementNamed(context, RouteNames.login);
+                  Navigator.pushReplacementNamed(context, RouteNames.login);
                 },
-                child: Icon(
+                child: const Icon(
                   CupertinoIcons.back,
                   size: 30,
                   color: Color(0xFF294B29),
@@ -44,7 +54,7 @@ class PasswordEmailResetPage extends StatelessWidget {
               left: 40,
               child: Container(
                 alignment: Alignment.centerLeft,
-                child: Text(
+                child: const Text(
                   "ENTER YOUR EMAIL TO\nRESET YOUR PASSWORD",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -62,46 +72,79 @@ class PasswordEmailResetPage extends StatelessWidget {
               child: Column(
                 children: <Widget>[
                   TextField(
-                    decoration: InputDecoration(
+                    controller: emailController,
+                    decoration: const InputDecoration(
                       labelText: "Enter the Email",
                     ),
                   ),
                   SizedBox(height: size.height * 0.05),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(80.0),
-                      ),
-                      padding: EdgeInsets.zero,
-                      textStyle: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 50.0,
-                      width: size.width * 0.5,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(80.0),
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xFF294B29),
-                            Color(0xFF50623A),
-                          ],
+                  BlocConsumer<AuthBloc, AuthState>(
+                    listener: (context, state) {
+                      if (!mounted) return; // Ensure the widget is still mounted
+
+                      if (state is ResetEmailSent) {
+                        floatingSnackBar(
+                          context: context,
+                          message: 'Reset email sent. Check your inbox.',
+                          backgroundColor: Colors.green,
+                        );
+                        Navigator.pushReplacementNamed(context, RouteNames.login);
+                      } 
+                    },
+                    builder: (context, state) {
+                      if (state is AuthStateLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      return ElevatedButton(
+                        onPressed: () {
+                          final email = emailController.text;
+                          if (email.isEmpty) {
+                            floatingSnackBar(
+                              context: context,
+                              message: 'Please enter your email',
+                              backgroundColor: Colors.redAccent,
+                            );
+                            return;
+                          }
+                          context.read<AuthBloc>().add(
+                                ForgotPasswordEvent(email: email),
+                              );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80.0),
+                          ),
+                          padding: EdgeInsets.zero,
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      padding: const EdgeInsets.all(0),
-                      child: Text(
-                        "SEND EMAIL",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50.0,
+                          width: size.width * 0.5,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(80.0),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFF294B29),
+                                Color(0xFF50623A),
+                              ],
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(0),
+                          child: const Text(
+                            "SEND EMAIL",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    },
                   ),
                 ],
               ),
