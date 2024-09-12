5ad4ecfa4063110e5700e214a835dd52faa6e6d6
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2024-09-12 07:40:47 +0530

    Fixed the errors

--- lib/pages/student_pages/profile/profile_update_screen.dart
+++ lib/pages/student_pages/profile/profile_update_screen.dart
@@ -70,7 +70,25 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
     }
   }
 
+  Route _createRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(-1.0, 0.0); // Start from left
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
 
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+      transitionDuration: const Duration(seconds: 1), // 1 second duration
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -256,3 +274,4 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
     );
   }
 }
+
